a114d2adb9ad19f542fb65e920cdf02a4b1eaa64
LuongHuuPhuc <Huuphuc1016@gmail.com>
2026-06-20 00:54:19 +0700

    Debug done. Turn of NEED_TO_DEBUG macros

--- components/common_helper/cm_macros.h
+++ components/common_helper/cm_macros.h
@@ -6,7 +6,7 @@
 #define __CM_MACROS_H
 
 #define UART_DRIVER_INSTALL_WITH_QUEUE    1   /* Sau khi install UART voi queue thi khong can xQueueCreate */
-#define NEED_TO_DEBUG                     1   /* Dung khi can debug chung */
+// #define NEED_TO_DEBUG                     1   /* Dung khi can debug chung */
 // #define UART_ESP_PRINTF_USING             1   /* Dung neu can su dung ham printf thu cong */
 // #define VERIFY_ASCII_MODE                 1   /* Debug data UART o dang ASCII */
 #define VERIFY_PACKET_MODE                1   /* Debug data UART o dang Binary packet */
